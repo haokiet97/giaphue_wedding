525a718965fbb5751fc9f90fc3aa9bc11cda9afa
haokiet97 <vanlahaokietxua@gmail.com>
2024-12-08 23:25:31 +0700

    4: fix carousel + implement album

--- src/app/components/home/carousel/carousel.component.ts
+++ src/app/components/home/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, AfterViewInit } from '@angular/core';
 import {FEMALE_NAME, MALE_NAME, CAROUSEL_DATA} from "../../../shared/constants";
 import {forEach} from "lodash";
 import {NgForOf} from "@angular/common";
@@ -13,9 +13,19 @@ import {NgForOf} from "@angular/common";
   styleUrl: './carousel.component.css'
 })
 export class CarouselComponent {
-  protected readonly carouselImages: string[] = CAROUSEL_DATA.images;
+  protected readonly carouselImages = CAROUSEL_DATA.images;
   protected readonly carouselVideo: string = CAROUSEL_DATA.video;
   protected readonly MALE_NAME = MALE_NAME;
   protected readonly FEMALE_NAME = FEMALE_NAME;
   protected readonly forEach = forEach;
+  protected isLandscape: boolean = false;
+
+  ngAfterViewInit() {
+    this.isLandscape = window.innerWidth > window.innerHeight;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.isLandscape = event.target.innerWidth > event.target.innerHeight;
+  }
 }
--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -32,16 +32,16 @@ export const FEMALE_NAME = 'Huệ';
 
 export const ABOUT_DATA = {
   maleData: {
-    image: 'assets/img/tuanhuyen/tuan.jpg',
-    desc: 'Chú rể là một lập trình viên đang công tác tại ngân hàng MB. Một người hiền lành, sống tình cảm và luôn yêu thương gia đình.',
+    image: 'https://lh3.googleusercontent.com/pw/AP1GczP76nVafQDQ8rqVSbPvYFCQzijd5RI9I0bJ86xXS3LXKKpjTvngUujLd0uNw4TdxD8NwplrMdDJvtLTLK6jA9CcHjN0ppb-jG6-44AZt3gpmQWsT-WcoeGY5OT2z6rWPd2hSBP95hXF-L_dmvaNG98=w697-h929-s-no-gm',
+    desc: 'Chú rể là một lập trình viên. Một người hiền lành, sống tình cảm, tuy đôi lúc hơi cứng nhắc nhưng luôn yêu thương gia đình và đặc biệt là cô dâu Kim Huệ.',
     fb: 'https://www.facebook.com/giaplinux/',
     zl: '',
     ins: '',
     tel: '0388966276'
   },
   femaleData: {
-    image: 'assets/img/tuanhuyen/huyen.jpg',
-    desc: 'Cô dâu đang làm việc tại phòng vé Minh Trang Travel. Là một người luôn vui vẻ, nhanh nhẹn và chịu khó. Luôn yêu thương chú rể Mạnh Tuấn.',
+    image: 'https://lh3.googleusercontent.com/pw/AP1GczPVdC5kTnmf_qhtc2b3BhSuMLcaRBEY7NYOORWY2PlIzWzFpdG0LHiBrXOWRjcjEMdMXIoOEEslf3bPV8FutxGCmOdq_3_33ZCRjvKxMGV-EULokSOjazzIM70TMYy-BoPkWXFRvAi3VB601VecZO4=w697-h929-s-no-gm',
+    desc: 'Cô dâu là một người luôn vui vẻ, nhanh nhẹn và chịu khó. Luôn yêu thương chồng.',
     fb: '',
     zl: '',
     ins: '',
@@ -53,8 +53,14 @@ export const SECRET_KEY = 'tuanhuyen_wedding';
 
 export const CAROUSEL_DATA = {
   images: [
-    "https://lh3.googleusercontent.com/pw/AP1GczNcKudpCuMlbEs8Vs3thAmJZ5X1cP6UjhwBQ07bZ0CBPCVCn97E0b4xsBs6tRwc5r6PMHb_J20-xMATm71NNdX2R5WoPtI5bHvVu_dylzieMuo2Yd2lVoU_h5432ZENiN9kl5dzlcZ4q0JZHO9S41Q=w619-h929-s-no-gm",
-    "https://lh3.googleusercontent.com/pw/AP1GczOerZKvSryWBF7cWXr_9meAVcsfFyx-chMFjP-idiABNlaH2yLbDQSvn-I_IDdShf3ac2xVzFsvfDp5XEDIEGRVHSpzut5XxGVV1rdO1GjLd6RDaKNZq5Y4Z7QujJk1xFVo_nXw-K4JXpN8Se4uY6I=w619-h929-s-no-gm"],
+    {url: "https://lh3.googleusercontent.com/pw/AP1GczMaI8GymHEHZoUbHNjeT0EugQbuKCbnX2UmHkw-h91y_50W6_oUoquG1ksCo7Bu5Y8l0Zlhk4XLJH6KQg5mDZ4h5X6TZsf6aTzz_9t3-qQ8xmQYlEWgBZvB1tyBFENuKhvm14AeeN8B1_BN0eXuTUg=w523-h929-s-no-gm",
+    landscapeUrl: "https://lh3.googleusercontent.com/pw/AP1GczNKiXp5K-cMSVV93upkpYaXuPJSUkNfBblcL4cG4LDojA7Rm3hNCw33XoKrDcQ6-VxiTlL9SjUSJEObYw9It1VFo6DA9XOO5-umjHGiYOLPDKYbcKZx7Vv9Vl6FeLExrMJgEX80FEISbcZIlmg5tGo=w1560-h878-s-no-gm"
+    },
+    {
+      url: "https://lh3.googleusercontent.com/pw/AP1GczMuewtFVn4Fp9QqX1UFue5bUPaGW3__wF6ymS8J2pDOdwoiI1uTNzf51EQMlC40w2lyLIoUR4EKZr0sf2CH4EdfA3qvuxbUcIuqQLLlMes2Tbu43rPGaoYHPbJfxadqA2whuq1ydRfCM9y2H0JmvLM=w619-h929-s-no-gm",
+      landscapeUrl: "https://lh3.googleusercontent.com/pw/AP1GczNnu-oGaLPOtIIyvSw1AHT4iGD7TzWgsQtqOAY78JHk47_aV1-yzq4p1pGUvEtLP0oXN0aZpmJf_ocVSsnn_TtyT2Zm5hagojMx_N_eJKZFb9DGpgZkf4jvzjCnzqQnwS5AQ0JUI-uA-nzocrzORzw=w1560-h877-s-no-gm"
+    }
+    ],
   video: "https://www.youtube.com/embed/_shTSNNBpmE?si=neQpum3B4cT6QS34"
 }
 // Thông tin địa điểm thời gian sự kiện
@@ -79,7 +85,7 @@ export const EVENT_DATA = {
 export const STORY_DATA = {
   quote: 'Hãy để tình yêu diễn biến thật tự nhiên, đã là duyên thì xa đến mấy cũng chẳng sợ lạc đường',
   title: 'Chuyện tình yêu chúng mình',
-  desc: 'Mình là chú rể Mạnh Tuấn, sau đây mình xin kể cho các bạn nghe về chuyện tình yêu của chúng mình',
+  desc: 'Hạnh phúc không phải là đích đến mà hạnh phúc là cả một quá trình.',
   timelines: [
     {
       title: 'Lần ngặp mặt đầu tiên',
@@ -101,16 +107,19 @@ export const MONEY_DATA = {
     bank: "MB Bank",
     account_name: "Nguyễn Văn Giáp",
     account: "0388966276",
-    qr_image: "assets/img/bank/tuan-bank.jpg"
+    qr_image: "https://lh3.googleusercontent.com/pw/AP1GczOzqHh1YUByX42qVXfcMg7Q-TdaLj21LE5OsTWkmnEm5ArZ-vfz0FmJZZbCCRmr8E82DcB5niAh4LQiGasbP5qxPXLiR4wzvS2lSAP-HuKo5RC-h9W96XZa3evJnusxG008hx7HR09RGIAsEn3l2wY=w696-h929-s-no-gm"
   },
   femaleData: {
     bank: "MB Bank",
     account_name: "Hoàng Thanh Kim Huệ",
     account: "0336135208",
-    qr_image: "assets/img/bank/huyen-bank.jpg"
+    qr_image: "https://lh3.googleusercontent.com/pw/AP1GczOzqHh1YUByX42qVXfcMg7Q-TdaLj21LE5OsTWkmnEm5ArZ-vfz0FmJZZbCCRmr8E82DcB5niAh4LQiGasbP5qxPXLiR4wzvS2lSAP-HuKo5RC-h9W96XZa3evJnusxG008hx7HR09RGIAsEn3l2wY=w696-h929-s-no-gm"
   }
 }
 
 export const GALLERY_DATA = {
-
+  images: [
+    "https://lh3.googleusercontent.com/pw/AP1GczPaGupJo5GwFv9SLWAA-v556VRVl9FKLACLIrRmQZi8lnos6twbUZHqXNTxAQ4zFktzwryFjwTS5eW1gzFuyre3uyrg4et3B4rtbseYCR0zLiUSJaUJaxuf2lS6XVKt3115oxkh29-39JLLZiXxyjgL=w619-h929-s-no-gm",
+    "https://lh3.googleusercontent.com/pw/AP1GczNl1Q0N-3RPDjUMorIdGz8XqwF29jJ2wk_O2bU23Wkc24KriTzC0FsFO_oOhsbGQ1g7Ve87DIYw55FDVOSE5zD689CapYzofzIaGvT9LMmq3Vo80yoNbUsVVzIeknCf5c8rtjgZnZUGPt73BQnh4xqF=w619-h929-s-no-gm"
+  ]
 }
